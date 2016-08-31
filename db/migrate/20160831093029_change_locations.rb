06c84c4e106ffecf93855620f8b94b9420c5fd96
FTAndy <fortheandy@gmail.com>
2016-08-31 17:55:01 +0800

    add gitignore

--- db/migrate/20160831093029_change_locations.rb
+++ db/migrate/20160831093029_change_locations.rb
@@ -1,6 +1,6 @@
 class ChangeLocations < ActiveRecord::Migration
   def change
     change_column :locations, :name, :string, null: false
-    change_column :locations, :code, :string
+    change_column :locations, :code, :string, null: true
   end
 end
--- db/migrate/20160831093302_change_locations_table.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLocationsTable < ActiveRecord::Migration
-  def change
-    change_column :locations, :code, :string, null: true
-  end
-end
